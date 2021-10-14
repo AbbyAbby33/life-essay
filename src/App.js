052138b2a37587fae100ba5cb25d41f84a029c95
Abby <sk573129@gmail.com>
2021-10-14 22:21:43 +0800

    feat: 新增頁籤

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 /** Ant Design */
 import { Layout, Menu } from 'antd';
+import { BookOutlined, AreaChartOutlined, MailOutlined } from '@ant-design/icons';
+
 const { Header, Content, Sider } = Layout;
+const { SubMenu } = Menu;
 
 export default class App extends Component {
   render() {
@@ -14,7 +17,20 @@ export default class App extends Component {
         <Content>
           <Layout className="site-layout-background">
             <Sider collapsible={true} collapsedWidth={0} breakpoint="lg">
-              <Menu className="le-menu">
+              <Menu className="le-menu" mode="inline">
+                <SubMenu key="sub1" icon={<BookOutlined/>} title="小事管理">
+                  <Menu.Item key="1">新增小事</Menu.Item>
+                  <Menu.Item key="2">小事列表</Menu.Item>
+                  <Menu.Item key="3">小事日曆</Menu.Item>
+                  <Menu.Item key="4">隨機小事</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<AreaChartOutlined/>} title="統計分析">
+                  <Menu.Item key="5">小事統計</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<MailOutlined />} title="關於專案">
+                  <Menu.Item key="6">技術資源</Menu.Item>
+                  <Menu.Item key="7">聯絡作者</Menu.Item>
+                </SubMenu>
               </Menu>
             </Sider>
             <Content className="le-main-content">Content</Content>
