4214488b8d56117f87a90080ef3a121eb4936993
Abby <sk573129@gmail.com>
2021-10-14 17:04:21 +0800

    feat: 使用antd拉出header、sider、content基本架構

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
+/** Ant Design */
+import { Layout, Menu } from 'antd';
+const { Header, Content, Sider } = Layout;
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <Layout>
+        <Header className="header">
+          <div>生活小事</div>
+        </Header>
+        <Content>
+          <Layout className="site-layout-background">
+            <Sider>
+              <Menu style={{ height: 'calc(100vh - 64px)' }}>
+              </Menu>
+            </Sider>
+            <Content style={{ padding: '24px', minHeight: 280 }}>Content</Content>
+          </Layout>
+        </Content>
+      </Layout>
     )
   }
 }
