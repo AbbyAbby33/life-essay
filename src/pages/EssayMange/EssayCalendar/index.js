08e3ed8f5bd6b68af71bb369c3b96380a0963742
Abby <sk573129@gmail.com>
2021-10-19 20:42:42 +0800

    feat: 1.修改共用假資料 2.日曆頁面開發：取每格日期的資料

--- src/pages/EssayMange/EssayCalendar/index.js
+++ src/pages/EssayMange/EssayCalendar/index.js
@@ -1,59 +1,124 @@
 import React, { Component } from 'react';
 import { PageHeader } from 'antd';
-import { Calendar, Badge } from 'antd';
+import { Calendar } from 'antd';
+import moment from 'moment';
+import 'moment/locale/zh-tw';
 
 export default class EssayCalendar extends Component {
-
-    getListData(value) {
-        // console.log('value', value);
-        // console.log('moment', moment('2021-09-21 12:00:00'));
-
-        let listData;
-        switch (value.date()) {
-            case 8:
-                listData = [
-                    { type: 'warning', content: 'This is warning event.' },
-                    { type: 'success', content: 'This is usual event.' },
-                ];
-                break;
-            case 10:
-                listData = [
-                    { type: 'warning', content: 'This is warning event.' },
-                    { type: 'success', content: 'This is usual event.' },
-                    { type: 'error', content: 'This is error event.' },
-                ];
-                break;
-            case 15:
-                listData = [
-                    { type: 'warning', content: 'This is warning event' },
-                    { type: 'success', content: 'This is very long usual event。。....' },
-                    { type: 'error', content: 'This is error event 1.' },
-                    { type: 'error', content: 'This is error event 2.' },
-                    { type: 'error', content: 'This is error event 3.' },
-                    { type: 'error', content: 'This is error event 4.' },
-                ];
-                break;
-            default:
-        }
-        return listData || [];
+    state = {
+        data: [
+            {
+                key: '1',
+                title: '慶祝母親節',
+                happiness: 3,
+                date: '2021-05-09 00:00:00',
+            },
+            {
+                key: '2',
+                title: '學習攝影',
+                happiness: 3,
+                date: '2021-08-02 00:00:00',
+            },
+            {
+                key: '3',
+                title: '幫薇薇過27歲生日',
+                happiness: 4,
+                date: '2021-08-16 00:00:00',
+            },
+            {
+                key: '4',
+                title: '獲得免費的桃子',
+                happiness: 5,
+                date: '2021-09-03 00:00:00',
+            },
+            {
+                key: '5',
+                title: '去台中出差',
+                happiness: 3,
+                date: '2021-09-06 00:00:00',
+            },
+            {
+                key: '6',
+                title: '煮薑母茶暖暖身子',
+                happiness: 3,
+                date: '2021-09-08 00:00:00',
+            },
+            {
+                key: '7',
+                title: '慵懶的午後',
+                happiness: 3,
+                date: '2021-09-12 00:00:00',
+            },
+            {
+                key: '8',
+                title: '到海邊玩耍',
+                happiness: 3,
+                date: '2021-09-20 00:00:00',
+            },
+            {
+                key: '9',
+                title: '下午茶',
+                happiness: 3,
+                date: '2021-09-22 00:00:00',
+            },
+            {
+                key: '10',
+                title: '陽台視角',
+                happiness: 3,
+                date: '2021-09-28 00:00:00',
+            },
+            {
+                key: '11',
+                title: '新的開始',
+                happiness: 3,
+                date: '2021-10-05 00:00:00',
+            },
+            {
+                key: '12',
+                title: '準備冬季衣物',
+                happiness: 3,
+                date: '2021-10-14 00:00:00',
+            },
+            {
+                key: '13',
+                title: '壽司',
+                happiness: 3,
+                date: '2021-10-17 00:00:00',
+            },
+            {
+                key: '14',
+                title: '冬天要來了',
+                happiness: 3,
+                date: '2021-10-22 00:00:00',
+            }
+        ]
     }
 
+    /** 取得每一格日期的資料 */
+    getListData(value) {
+        const cellTime = moment(value).startOf('date').valueOf();
+        const cellEssayList = this.state.data.filter(v => {
+            const essayTime = moment(v.date).valueOf();
+            return cellTime === essayTime;
+        });
+        // console.log('cellEssayList', cellEssayList);
+        return cellEssayList || [];
+    }
+
+    /** 取得每一格日期要展示的view */
     dateCellRender = (value) => {
         const listData = this.getListData(value);
         // console.log('listData', listData);
         return (
             <ul className="events">
                 {listData.map(item => (
-                    <li key={item.content}>
-                        <Badge status={item.type} text={item.content} />
+                    <li key={item.title}>
+                        {/* <Badge status={item.type} text={item.title} /> */}
+                        {item.title}
                     </li>
                 ))}
             </ul>
         );
-    }
-
-    onChange = (date, dateString) => {
-        console.log(date, dateString);
     }
 
     render() {
